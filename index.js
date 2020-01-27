63240a33cbc04cece54bfece13598d615fbb3cb2
Bram Esposito <bram@bramesposito.com>
2020-01-27 16:46:24 +0100

    First version of Harvest Auth

---
+++ index.js
@@ -0,0 +1,51 @@
+/*
+Authenticate on the v2 version of the Harvest API
+ */
+const express           = require('express');
+const session           = require('express-session');
+const grant             = require('grant-express');
+const request           = require('request');
+
+module.exports = function(data,callback) {
+    const app = express();
+
+    app.use(session({secret: data.APP_SECRET, saveUninitialized: true, resave: true}));
+    app.use(grant({
+        defaults: {
+            protocol: data.PROTOCOL,
+            host: data.HOST
+        },
+        harvestv2: {
+            "authorize_url": "https://id.getharvest.com/oauth2/authorize",
+            "access_url": "https://id.getharvest.com/api/v2/oauth2/token",
+            "oauth": 2,
+            key: data.CLIENT_ID,
+            secret: data.CLIENT_SECRET,
+            "callback": "/auth/harvestv2"
+        }
+    }));
+
+    app.get('/auth/harvestv2', (req, res) => {
+
+        const options = {
+            url: 'https://id.getharvest.com/api/v2/accounts',
+            headers: {
+                'Authorization': 'Bearer ' + req.query.access_token,
+                'User-Agent': data.APP_NAME
+            },
+            json:true
+        };
+
+        request(options, (qerr, qres, qbody) => {
+
+            if (qerr) {
+                res.send("Error requsting harvest accounts: "+qerr);
+                return console.log(qerr);
+            }
+
+            callback(res, {tokens: req.query, accounts: qbody});
+        });
+    });
+
+    return app;
+};
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
 # Authenticate via the Harvest API
+
+## Requirements
+
+You need [Express](https://expressjs.com/) to use this module 
+
+## How to use
+
+Install using npm: "npm i harvest-auth"
+
+Take a look at at the [example implementation]("examples/existing-express.js")
