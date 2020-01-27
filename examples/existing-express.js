b04d210ceff0814bdac7c76d118856ac7255ce1e
Bram Esposito <bram@bramesposito.com>
2020-01-27 17:11:27 +0100

    clean up example

---
+++ examples/existing-express.js
@@ -0,0 +1,23 @@
+const express           = require('express');
+const harvestAuth       = require('harvest-auth');
+
+let app             = express();
+const port          =  3000;
+
+app.use(harvestAuth({
+    APP_NAME: "FILL IN HERE",
+    APP_SECRET: "keyboard cat",
+    PROTOCOL: "http",
+    HOST: "localhost:3000",
+    CLIENT_ID: 'FILL IN HERE',
+    CLIENT_SECRET: 'FILL IN HERE'
+    },
+    function(res, data) {
+        // callback after authentication
+        console.log(data);
+        res.send(JSON.stringify(data, null, 2));
+    }
+));
+
+app.listen(port);
+console.log('Farmhand API is listening on port ' + port);
